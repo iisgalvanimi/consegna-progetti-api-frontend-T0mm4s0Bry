848ff83af80e77ea3b81edc298c565abfa0f52b3
T0mm4s0Bry brignoli <brignoli.tommaso@iisgalvanimi.edu.it>
2024-11-12 12:49:42 +0000

    parte 2

--- Animali_Frutta/src/app/app-routing.module.ts
+++ Animali_Frutta/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimaliComponent } from './animali/animali.component';
 import { FruttaComponent } from './frutta/frutta.component';
+import { GenericComponent } from './generic/generic.component';
 
 const routes: Routes = [
+  { path: 'generic/:id', component: GenericComponent },
   {path: '', redirectTo: '/animali', pathMatch: 'full'},
   {path: 'Animali', component: AnimaliComponent},
   {path: 'Frutta', component: FruttaComponent},
--- Animali_Frutta/src/app/generic/generic.component.ts
+++ Animali_Frutta/src/app/generic/generic.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Animali, Emoji, Frutta } from "../emoji";
 
-@Component({
-  selector: 'app-generic',
-  templateUrl: './generic.component.html',
-  styleUrls: ['./generic.component.css']
-})
-export class GenericComponent {
-
+export class GenericComponent{
+  //Crea un vettore di tipo Emoji e lo inizializza
+  genVect : Emoji[]= [{name:"", emoji:""}];
+  //Riceve l'oggetto ActivatedRoute come dependency injection
+  constructor(private route: ActivatedRoute) {
+    //Gestisce i cambi di route con l'observable paramMap
+    this.route.paramMap.subscribe(this.getRouterParam);
+  }
+ 
+  //Ogni volta che viene invocata la route tracks/:id, l'observable paramMap richiama questo metodo
+  getRouterParam = (params: ParamMap) =>
+  {
+    let uri_param = params.get('id'); 
+    console.log (uri_param); 
+    
+    if (uri_param == 'fruits') this.genVect = Frutta;
+    if (uri_param == 'animals') this.genVect = Animali;
+  }
 }
