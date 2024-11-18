a8ca60c7605c21ef3498953ebfc2774bbbbdb1ec
T0mm4s0Bry brignoli <brignoli.tommaso@iisgalvanimi.edu.it>
2024-11-18 13:54:31 +0000

    finito2

--- magic-card-API/src/app/services/magic-card.service.ts
+++ magic-card-API/src/app/services/magic-card.service.ts
@@ -11,18 +11,16 @@ export class MagicCardService {
 
   constructor(private http: HttpClient) {}
 
-  // Metodo per ottenere una carta per nome
+
   getCard(name: string): Observable<MagicCard> {
     return this.http.get<MagicCard>(`${this.baseUrl}/named?exact=${name}`);
   }
 
-  // Metodo per ottenere carte filtrate per anno
   getCardsByYear(year: string): Observable<any> {
     const url = `${this.baseUrl}/search?q=year:${year}`;
     return this.http.get<any>(url);
   }
 
-  // Metodo per cercare una carta per nome
   searchCardByName(name: string): Observable<MagicCard> {
     return this.http.get<MagicCard>(`${this.baseUrl}/named?fuzzy=${name}`);
   }
